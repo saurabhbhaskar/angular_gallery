44358eac6b5e7235331359d7f0aa8dae88883d60
Saurabh Bhaskar <82755231+saurabhbhaskar@users.noreply.github.com>
2024-05-23 22:02:38 +0530

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   appname: 'angular_gallery',
   supabase: {
-    url: 'https://eegmfuapbmpnucjenscs.supabase.co',
-    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVlZ21mdWFwYm1wbnVjamVuc2NzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY0NTcyOTIsImV4cCI6MjAzMjAzMzI5Mn0.dqQic4hFAMS0T-CIEn1AG-j-NlycR8Uc-QUTASaisuE',
+    url: '',
+    key: '',
   },
 };
